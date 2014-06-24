49fcba5d6d36c4d42c580de239e9a54a7db7c886
Annael93 <anncz93@gmail.com>
2014-06-24 12:07:03 +0200

    projektnowe
    dobrze

---
+++ ber.m
@@ -0,0 +1,14 @@
+function [  ] = ber(  )
+    figure();
+    SNR = -4:12;
+    EbN0=SNR(1):0.001:SNR(length(SNR));
+    error = (1/2)*erfc(sqrt(10.^(EbN0/10)));
+    sError  =  importdata('matlab.mat');
+    semilogy(SNR,sError,'*r',...
+                   EbN0 ,error,'green');
+    title('BER')
+    xlabel('SNR [dB]');
+    ylabel('BER')
+    legend('BPSK Symulacja','BPSK Teoria')
+   
+end
